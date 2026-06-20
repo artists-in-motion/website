a883049ca13280c2d5f5ca735b6bafc82c298ae8
artists-in-motion <karl@karlreynolds.com>
2026-06-20 17:42:27 +1000

    Update vis-3.js

--- vis-3.js
+++ vis-3.js
@@ -1,4 +1,4 @@
-console.log("Vis 3 - SAT 20th JUN v1");
+//console.log("Vis 3 - SAT 20th JUN v1");
 (function startWhenAIMReady() {
   if (!window.AIM) {
     window.addEventListener("aimGlobalReady", startWhenAIMReady, {
